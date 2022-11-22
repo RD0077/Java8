bd3ab86aa0ac7b3f3272667e479930682dbb222a
ASUS <rahul.dhotkar143@gmail.com>
2022-11-22 18:51:33 +0530

    NA

--- StringPrograms/src/main/java/com/stringEx/StringAnagram.java
+++ StringPrograms/src/main/java/com/stringEx/StringAnagram.java
@@ -3,8 +3,26 @@ package com.stringEx;
 public class StringAnagram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(isAnagram("aabbc","baaaaaa"));
+	}
+
+	private static boolean isAnagram(String string1, String string2) {
+		int count[]= new int[256];
+		for(int i=0;i<string1.length();i++)
+		{
+			count[string1.charAt(i)]++;
+			count[string2.charAt(i)]--;
+		}
+		for(int i=0;i<count.length;i++)
+		{
+			if(count[i]!=0)
+			{
+				return false;
+			}
+			
+		}
+		return true;
 	}
 
 }
